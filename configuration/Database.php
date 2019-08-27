6535eb16b10979ed8650cf6870e914ad10da6d08
ronjamyhr <ronja_myhr@hotmail.com>
2019-08-27 11:08:14 +0200

    add api for bookings and folders

---
+++ configuration/Database.php
@@ -0,0 +1,22 @@
+<?php 
+	class Database {
+    
+	private $host = 'localhost';
+	private $username = 'root';
+	private $dbName = 'silva';
+	private $password = 'root';
+	private $connection;
+
+	// DB Connect
+	public function connect() {
+		$this->connection = null;
+		try { 
+			$this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->dbName, $this->username, $this->password);
+			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch(PDOException $e) {
+			echo 'Connection Error: ' . $e->getMessage();
+		}
+		return $this->connection;
+	}
+}
+
---
+++ models/Booking.php
@@ -0,0 +1,50 @@
+<?php 
+	class Booking {
+
+	private $connection;
+	private $table = 'booking';
+
+	// Booking Properties
+	public $id;
+	public $customer_id;
+	public $date;
+	public $time;
+	public $number_of_guests;
+	public $customer_name;
+
+	// Constructor with DB
+	public function __construct($db) {
+		$this->connection = $db;
+	}
+
+	// Get Bookings
+	public function read() {
+
+		// Create query
+		$query = 'SELECT 
+			c.name as customer_name, 
+			b.id, 
+			b.customer_id, 
+			b.date, 
+			b.time, 
+			b.number_of_guests 
+			FROM 
+			' . $this->table . ' b
+			LEFT JOIN
+			customer c ON b.customer_id = c.id
+			ORDER BY
+			b.date DESC';
+
+		// Prepare statement
+		$stmt = $this->connection->prepare($query);
+
+		// Execute query
+		$stmt->execute();
+
+		return $stmt;
+	}
+}
+
+
+
+
